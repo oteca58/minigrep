bf76b68d2a62aa17800551731db8ec0a65b74f17
oteca58 <oteca@protonmail.com>
2023-01-02 11:43:09 +0100

    first issue

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+use std::env;
+use std::process;
+
+use minigrep::Config;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing argouments: {err}");
+        process::exit(1);
+    });
+    
+    println!("Seraching for {} ", config.query);
+    println!("in file {} ", config.file_path);
+
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {e}");
+        process::exit(1);
+    }
+
+}
+
+
